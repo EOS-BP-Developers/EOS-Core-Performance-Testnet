054db9388251fc32122f21f16bd5cf2ce18cef99
Jae Chung <jae.chung99@gmail.com>
2018-07-04 21:42:42 +0800

    Create params.sh

---
+++ params.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+EOS_SOURCE_DIR="/path/to/eos"
+
+API_PORT="9999"
+P2P_PORT="9876"
+NODE_IP=""
+
+EOS_PRODUCER_NAME=""
+ACCOUNT_PUB_KEY=""
+ACCOUNT_PRIV_KEY=""
+# Signing key can be the same as account keys. This is just a performance testnet.
+SIGNING_PUBLIC_KEY=""
+SIGNING_PRIV_KEY=""
+
+WALLET_HOST="127.0.0.1"
+WALLET_PORT="8888"
+
+NODE_BIN=""
+KEOSD_BIN=""
+CLEOS_BIN=""
